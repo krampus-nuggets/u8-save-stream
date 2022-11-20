64d6627d11f7a2630cc60c2f4a02f05b5d664c2a
Brandon Williams <brandon@wemakeart.co.za>
2022-11-20 13:03:51 +0200

    update: apply valid streams check

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 
 # Internal Imports
 from modules.engine import download_all_streams
-from modules.utils import check_config, read_config, term_output, is_windows
+from modules.utils import check_config, read_config, term_output, is_windows, get_filenames, get_valid_streams
 
 
 root_dir = os.path.dirname(__file__)
@@ -25,6 +25,17 @@ if (config_data == None):
 
 # Get stream data in config
 streams = config_data["streams"]
+
+# Get saved stream names
+filenames = get_filenames(root_dir, "mp4")
+
+# Get streams that were missed
+unsaved_streams = get_valid_streams(filenames, streams)
+
+# CHECK - Valid list returned containing stream dictionaries
+if (unsaved_streams == None):
+  term_output("error", "Failed to retrieve valid streams")
+  sys.exit(1)
 
 # Download Streams in config file
 try:
